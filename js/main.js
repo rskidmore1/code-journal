6f8b8b02e7fdbb7bb1563d0b33f2301cbd7c2647
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-14 13:16:44 -0700

    Returns entries from dom

--- js/main.js
+++ js/main.js
@@ -33,3 +33,45 @@ function submitEntry(event) {
 }
 
 submitForm.addEventListener('submit', submitEntry);
+
+function retrieveEntry(entry) {
+  var entryLi = document.createElement('li');
+
+  var entryRow = document.createElement('div');
+  entryRow.className = 'row';
+  var entryColHalfImg = document.createElement('div');
+  entryColHalfImg.className = 'column-half';
+  var entryImg = document.createElement('img');
+  entryImg.className = 'placeholder-img rounded form-item-div';
+  // replace src when time is right
+  entryImg.setAttribute('src', entry.photoUrl);
+  entryImg.setAttribute('alt', 'placeholder');
+
+  var entryColHalfText = document.createElement('div');
+  entryColHalfText.className = 'column-half';
+  var entryMargin = document.createElement('div');
+  entryMargin.className = 'form-item-div input-bottom-margin';
+  var entryH2 = document.createElement('h2');
+  entryH2.textContent = entry.title;
+  var entryText1 = document.createElement('p');
+  entryText1.textContent = entry.notes;
+
+  entryLi.appendChild(entryRow);
+  entryRow.appendChild(entryColHalfImg);
+  entryColHalfImg.appendChild(entryImg);
+
+  entryRow.appendChild(entryColHalfText);
+  entryColHalfText.appendChild(entryMargin);
+  entryMargin.appendChild(entryH2);
+  entryMargin.appendChild(entryText1);
+
+  return entryLi;
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  var insert = document.querySelector('ul');
+
+  for (var i = 0; i < data.entries.length; i++) {
+    insert.appendChild(retrieveEntry(data.entries[i]));
+  }
+});
