c0d3fb84a7fa08d78d203b60bdfb53a256c1c72c
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-13 19:49:09 -0700

    Forgot to add data.js

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousFormEntries = localStorage.getItem('entry-form');
+
+if (previousFormEntries !== null) {
+  data = JSON.parse(previousFormEntries);
+}
+
+if (previousFormEntries === null) {
+  localStorage.setItem('entry-form', JSON.stringify(data));
+}
